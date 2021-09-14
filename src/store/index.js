85e0a22dc50cc9828bd3ae5e4d0eff0bf5da76ec
v_weibolai <v_weibolai@tencent.com>
2021-09-14 16:15:14 +0800

    完善

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,22 @@
 import { createStore } from 'vuex'
 
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+const modulesFiles = require.context('./modules', true, /\.js$/)
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
+
 export default createStore({
   state: {
-    pageBtns: [],
-    token: localStorage.getItem('token') || ''
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
-  },
   getters: {
-  }
+  },
+  modules
 })
---
+++ src/store/modules/user.js
@@ -0,0 +1,9 @@
+export default {
+  state: {
+    token: localStorage.getItem('token') || '',
+    permission: [],
+    user: {}
+  },
+  mutations: {},
+  actions: {}
+}
